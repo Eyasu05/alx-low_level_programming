d17bb0a069bf166fe2939f7143f18f07427970fa
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-11 14:50:19 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 /* hello world */
-int main(void) {  char str1[] = "with proper grammar, but the outcome is a piece of art,";
-  printf(str1);
+int main(void) {
+  printf("with proper grammar, but the outcome is a piece of art,");
   return (0);
 }
 
