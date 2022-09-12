7b076fd3d34af0962ebddae605393eb77f01128c
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-12 15:19:15 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 /* hello world */
-int main(void) {  char str1[] = "\"Programming is like building a multilingual puzzle";
+int main(void) {  
+  char str1[] = "\"Programming is like building a multilingual puzzle";
   puts(str1);
   return (0);
 }
