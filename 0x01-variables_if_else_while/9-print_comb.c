dea1e78318d5479ac0152cfb965a99b17c56cf58
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-12 22:49:55 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+int main()
+{
+  
+    // Get the character to be written
+    char ch = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9';
+  
+    // Write the Character to stdout
+        putchar(ch);
+  
+    return (0);
+}
