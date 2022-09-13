6c8fa821eca11aae5dd1f1e1e8e7270d08455335
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-13 14:50:32 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
-	putchar("_putchar");
-	return(0);
+  
+int main()
+{
+  
+    // Get the character to be written
+    char ch = "_putchar";
+  
+    // Write the Character to stdout
+    putchar(ch);
+  
+    return (0);
 }
