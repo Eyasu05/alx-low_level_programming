223fd92700348cc48f23c6ddd6df2b5cd5822bdb
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-19 21:14:58 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-#include <stdio.h>
 /**
  * 
  * modif_my_param - set the integer to 98
@@ -9,45 +7,8 @@
  * Return: nothing
  */
 void reset_to_98(int *n)
-
 {
-
 	   printf("Value of 'm': %p\n", m);
-
 	      printf("Address of 'm': %p\n", &m);
-
 	         *m = 98;
-
 }
-
-
-/**
- *
- *  * main - check the code 
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-int main(void)
-
-{
-
-	    int n;
-
-
-
-	        n = 402;
-
-		    printf("n=%d\n", n);
-
-		        reset_to_98(&n);
-
-			    printf("n=%d\n", n);
-
-			        return (0);
-}
-
-
