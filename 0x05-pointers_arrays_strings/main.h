1cbb5e7f8824bb5c6fe60a4eb0a06d892a70f25b
Eyasu05 <110991916+Eyasu05@users.noreply.github.com>
2022-09-19 21:34:57 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
 /*
 * _putchar -> putchar function
 * reset_to_98 -> reset_to_98 function
 */
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c)
 void reset_to_98(int *n)
 #endif
